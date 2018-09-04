c4a32f6692b6767437d6c9c59f7904f8689322fc
Naggerknife <messer185@gmail.com>
2018-09-04 18:42:58 -0400

    move folders around

---
+++ Assets/Scripts/Laser2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser2 : MonoBehaviour 
+{
+
+
+	private void Update()
+	{
+		ShootLaser();
+	}
+
+	private void ShootLaser()
+	{
+		
+	}
+}
